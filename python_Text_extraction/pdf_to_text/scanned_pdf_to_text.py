0caed1dc82db1259ada50b123139abc07befc63d
bagci <bagci@em.uni-frankfurt.de>
2023-08-15 08:44:10 +0200

    Update Landtag OCR

--- python_Text_extraction/pdf_to_text/scanned_pdf_to_text.py
+++ python_Text_extraction/pdf_to_text/scanned_pdf_to_text.py
@@ -251,14 +251,15 @@ if __name__ == "__main__":
     older_input = [
         # "Alter Landtag Württemberg (1797-1799)",
         # "Landtag Baden-Württemberg (1953-1996)",
-        # "Landtag Württemberg",
+        "Landtag Württemberg/Zweite Kammer (1820-1847, 1848-1918, 1920-1933)",
+        # "Landtag Württemberg/Erste Kammer (1820-1847, 1848-1918)",
         # "Landtag Württemberg-Baden (1946-1952)",
         # "Landtag Württemberg-Hohenzollern (1946-1952)",
         # "Ständeversammlung Württemberg (1815-1819)",
-        "Verfassungsgebende Landesversammlung Baden-Württemberg",
-        "Verfassungsgebende Landesversammlungen Württemberg (1849-1850, 1919-1920)",
-        "Verfassungsgebende Landesversammlung Württemberg-Baden",
-        "Verfassungsgebende Landesversammlung Württemberg-Hohenzollern"
+        # "Verfassungsgebende Landesversammlung Baden-Württemberg",
+        # "Verfassungsgebende Landesversammlungen Württemberg (1849-1850, 1919-1920)",
+        # "Verfassungsgebende Landesversammlung Württemberg-Baden",
+        # "Verfassungsgebende Landesversammlung Württemberg-Hohenzollern"
 
     ]
     for i in older_input:
