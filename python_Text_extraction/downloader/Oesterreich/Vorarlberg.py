be899259484340469091a2d941cd68a607b0f097
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2023-08-21 09:04:44 +0200

    Vorarlberg

--- python_Text_extraction/downloader/Oesterreich/Vorarlberg.py
+++ python_Text_extraction/downloader/Oesterreich/Vorarlberg.py
@@ -1,5 +1,7 @@
 import os
 import time
+
+import numpy as np
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,6 +11,8 @@ import urllib
 import pandas as pd
 from urllib import request
 from urllib.parse import quote
+import json
+from tqdm import tqdm
 
 def bs4_downloader(html_dir):
     with open(html_dir, "r") as f:
@@ -27,20 +31,37 @@ def download_pdf(file_name, url):
 
 
 def downlaod_from_tsv(tsv_dir):
-    download_dir = f"/home/staff_homes/bagci/data/temp"
+    download_dir = f"/home/bagci/data/tmp"
     df = pd.read_csv(tsv_dir, sep="\t", encoding="utf-8", header=None)
-    for index, row in df.iterrows():
+    failed = []
+    for index, row in tqdm(df.iterrows()):
         name = row[0]
-        if name is None or name == "":
+        if name is None or name == "" or name is np.nan:
             continue
         year = int(row[1])
         link = row[2]
         link_new = link.split("?OpenDocument")[0]
         url_enconde_name = quote(name, safe="")
         link_new = f"{link_new}/$FILE/{url_enconde_name}.pdf"
+        other_link = row[3]
         file_dir = f"{download_dir}/{year}/{name}.pdf"
-
-        download_pdf(file_dir, link_new)
+        try:
+            download_pdf(file_dir, link_new)
+        except Exception as ex:
+            try:
+                download_pdf(file_dir, other_link)
+            except Exception as ex1:
+                try:
+                    file_name_i = quote(other_link.split("/")[-1].split(".pdf")[0], safe="")
+                    start_url = other_link.replace(other_link.split("/")[-1], "")
+                    alternative_link = f"{start_url}{file_name_i}.pdf"
+                    download_pdf(file_dir, alternative_link)
+                except Exception as ex:
+                    failed.append(f"{year}-{name}")
+                    print(ex)
+    os.makedirs(os.path.dirname(f"/storage/projects/bagci/test"), exist_ok=True)
+    with open(f"/storage/projects/bagci/test", "w", encoding="UTF-8") as json_file:
+        json.dump(failed, json_file, indent=2)
     print("h")
 
 
@@ -74,4 +95,4 @@ if __name__ == '__main__':
     # bs4_downloader(test)
     # page_name = f"https://vorarlberg.at/web/landtag/lis"
     # download_protokoll(test)
-    downlaod_from_tsv(f"~/data/temp/vorarlberg.tsv")
+    downlaod_from_tsv(f"vorarlberg.tsv")
